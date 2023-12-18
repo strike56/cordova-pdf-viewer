97dc3d855b5bc2880315e3fa0c68c4853db02fe9
strike56 <strike56@gmail.com>
2023-12-18 20:55:52 +0200

    Update hookR.js

--- hooks/hookR.js
+++ hooks/hookR.js
@@ -29,7 +29,7 @@ filestoreplace.forEach(function(val, index, array) {
     if (fs.existsSync(val)) {
         console.log("Android platform available !");
         //Getting the package name from the android.json file,replace with your plugin's id
-        var packageName = configobj.installed_plugins["cordova-incidious-barcodescanner"]["PACKAGE_NAME"];
+        var packageName = configobj.installed_plugins["cordova-pdf-viewer"]["PACKAGE_NAME"];
         console.log("With the package name: " + packageName);
         console.log("Adding import for R.java");
         replace_string_in_file(val, "package com.pdfviewer;", "package com.pdfviewer;\n\nimport " + packageName + ".R;");
